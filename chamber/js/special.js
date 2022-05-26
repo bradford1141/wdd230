689a594d2dd12bf4ba753eb856005f05884e5eb3
Tyson Bradford <bradford1141@gmail.com>
2022-05-26 16:55:02 -0600

    special

--- chamber/js/special.js
+++ chamber/js/special.js
@@ -2,11 +2,7 @@ let date = new Date();
 
 const d = new Date();
 
-// function dayOfWeek(){
-//     document.querySelector("#message").textContent = "Come join us for a chamber meeting on Wednesday at 7:00pm"
-// }
-
-if(date.getDate() == 1 || date.getDate() == 2) {
+if(date.getDay() == 1 || date.getDay() == 2) {
     document.querySelector('#message').style.display = "block";
 }
 else{
