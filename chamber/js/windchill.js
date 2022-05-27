7ab72266ab1434bfd85240d4e833ef4fbc734d62
Tyson Bradford <bradford1141@gmail.com>
2022-05-26 23:00:39 -0600

    half windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,2 +1,6 @@
 temperature = parseFloat(document.querySelector('#temp').textContent);
 windspeed = parseFloat(document.querySelector('#windspeed').textContent);
+
+chill=(0.0817*(3.71*(Math.pow(wind, 0.5))+
+5.81-0.25*wind)*(temp-91.4)+91.4);
+form.windchill.value = chill;
