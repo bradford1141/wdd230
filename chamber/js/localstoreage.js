bda0809066ee10e8a3952c78f83ae1203fc0c01e
Tyson Bradford <bradford1141@gmail.com>
2022-05-31 15:46:09 -0600

    updated code

---
+++ chamber/js/localstoreage.js
@@ -0,0 +1,18 @@
+let lastVistDate;
+        const now = Date.now();
+
+        if (window.localStorage.getItem('last-visit-date'))
+        {
+            lastVistDate = window.localStorage.getItem('last-visit-date');
+        } else{
+            lastVistDate = now;
+        }
+
+        window.localStorage.setItem('last-visit-date', now);
+
+        const secondsSinceLastVisit = Math.floor((now - lastVistDate) / 1000);
+        const daysSinceLastVisit = Math.floor(secondsSinceLastVisit / 86400);
+        console.log(secondsSinceLastVisit);
+
+        document.querySelector('#seconds-ago').textContent = secondsSinceLastVisit;
+        document.querySelector('#days-ago').textContent = daysSinceLastVisit;
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,19 +1,14 @@
-temperature = parseFloat(document.querySelector('#temp').textContent);
-windspeed = parseFloat(document.querySelector('#windspeed').textContent);
+// temperature = parseFloat(document.querySelector('#temp').textContent);
+// windspeed = parseFloat(document.querySelector('#windspeed').textContent);
 
-chill=(0.0817*(3.71*(Math.pow(wind, 0.5))+
-5.81-0.25*wind)*(temp-91.4)+91.4);
-form.windchill.value = chill;
+const fahrenheit = (temp *9) / 5 +32;
+const windSPeedKmh = speed * 3.6;
+const windSpeedMph = speed * 2.237;
+const windChill = 35.74 + 0.6215 * fahrenheit - 35.75 * windSpeedMph**0.16 + 0.4275 * farenheit * windSpeedMph**0.1;
 
-// Math.round(
-//     35.74 +
-//       0.6215 * temperature -
-//       35.75 * windSpeed ** 0.16 +
-//       0.4275 * temperature * windSpeed ** 0.16
-//   );
-
-// const windChillFahrenheit = (temperature, windSpeed) =>
-//   35.74 +
-//   0.6215 * temperature -
-//   35.75 * windSpeed ** 0.16 +
-//   0.4275 * temperature * windSpeed ** 0.16;
+if(farenheit <= 50 && windspeed > 3){
+    wind.textContent = `${windChill.toFixed(2)} F`;
+}
+else{
+    wind.textContent = "N/A";
+}
