ff26c7b5eefef0a95f3f0ef3e06559f6cfca411d
Tyson Bradford <bradford1141@gmail.com>
2022-06-10 11:36:58 -0600

    join site

---
+++ chamber/js/localstoreage2.js
@@ -0,0 +1,18 @@
+let lastVistDate; 
+const RightNow = Date.now();
+
+if (window.localStorage.getItem('last-visit-date'))
+{
+    lastVistDate = window.localStorage.getItem('last-visit-date');
+} else{
+    lastVistDate = RightNow;
+}
+
+window.localStorage.setItem('last-visit-date', RightNow);
+
+const secondsSinceLastVisit = Math.floor((RightNow - lastVistDate) / 1000);
+const daysSinceLastVisit = Math.floor(secondsSinceLastVisit / 86400);
+console.log(secondsSinceLastVisit);
+
+document.querySelector('#seconds-ago2').textContent = secondsSinceLastVisit;
+document.querySelector('#days-ago2').textContent = daysSinceLastVisit;
