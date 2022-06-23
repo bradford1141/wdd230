3715b46bf8f4f5ac4190f97eedf9288a4da34486
Tyson Bradford <bradford1141@gmail.com>
2022-06-23 17:22:09 -0600

    api

---
+++ chamber/js/directory.js
@@ -0,0 +1,39 @@
+const resultElement = document.querySelector("#bussiness");
+fetch('')
+.then(response => response.json())
+.then(businesses => {
+    console.log(businesses);
+
+    businesses = businesses.directory;
+    businesses.forEach(business => {
+        const section = document.createElement('section');
+        section.setAttribute('class', 'card')
+
+        const bname = document.createElement('h1');
+        bname.textContent = `${business["business"]}`;
+
+        const location = document.createElement('h2');
+        location.textContent = `Location: ${business["address"]}`;
+
+        const email = document.createElement('p');
+        email.textContent = `Email: ${business["email"]}`;
+
+        const website = document.createElement('p');
+        website.textContent = `Website: ${business["website"]}`;
+
+        const image = document.createElement('img');
+        image.setAttribute('src', `${business["image"]}`);
+        image.setAttribute('alt', `${business["business"]}`);
+        image.setAttribute('class', 'logos')
+
+        section.appendChild(bname);
+        section.appendChild(location);
+        section.appendChild(contact);
+        section.appendChild(email);
+        section.appendChild(website);
+        section.appendChild(image);
+        resultElement.appendChild(section);
+
+    })
+
+})
---
+++ inclass/prophets/prophet.js
@@ -0,0 +1,25 @@
+const resultElement = document.querySelector("#prophets");
+fetch('https://byui-cit230.github.10/canvas-referenced/latter-day-prophets.json")
+.then (response => response.json())
+.then( prophets => {
+    console.log (prophets);
+
+    prophets = prophets.prophets;
+    prophets.forEach( prophet => {
+    const section = document.createElement ('section');
+    section.setAttribute('id', '')
+    const pname = document.createElement ('h1');
+    pname.textContent = `${prophet["name" ]} ${prophet["lastname"]}`;
+    const birthdate = document.createElement('h2');
+    birthdate.textContent = `${prophet["birthdate"]}`;
+    const birthplace = document.createElement('h3');
+    birthplace.textContent = `${prophet["birthplace"]}`;
+    const pimg = document.createElement('img');
+    pimg.setAttribute('src', `${prophet["imageurl"]}`);
+    pimg.setAttribute('alt', `${prophet["name"]}`);
+    section.appendChild(pname);
+    section.appendChild (birthdate);
+    section. appendChild(birthplace);
+    section.appendChild(pimg);
+    resultElement.appendChild(section);})
+})
