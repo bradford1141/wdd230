ad329cc1adf37490fe6a1d194fcd8c8b3b7a7ca2
Tyson Bradford <bradford1141@gmail.com>
2022-06-28 09:57:00 -0600

    windchill

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -24,3 +24,15 @@ fetch(url)
     weatherIcon.setAttribute('src', base_image_url);
     weatherIcon.setAttribute('alt', data.weather[0].description);
 });
+
+let temperature = parseFloat(document.querySelector('#current-temp').textContent);
+let windspeed = parseFloat(document.querySelector('#windspeed').textContent);
+
+if(temperature <= 50 && windspeed > 3){
+    const windChill = 35.74 + 0.6215 * temperature - 35.75 * windspeed**0.16 + 0.4275 * temperature * windspeed**0.1;
+    document.querySelector('#windchill').textContent = windChill.toFixed(2);
+    console.log(temperature);
+}
+else{
+    document.querySelector('#windchill').textContent = 'N/A';
+}
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,14 +1,11 @@
-// temperature = parseFloat(document.querySelector('#temp').textContent);
-// windspeed = parseFloat(document.querySelector('#windspeed').textContent);
+// let temperature = parseFloat(document.querySelector('#current-temp').textContent);
+// let windspeed = parseFloat(document.querySelector('#windspeed').textContent);
 
-const fahrenheit = (temp *9) / 5 +32;
-const windSPeedKmh = speed * 3.6;
-const windSpeedMph = speed * 2.237;
-const windChill = 35.74 + 0.6215 * fahrenheit - 35.75 * windSpeedMph**0.16 + 0.4275 * farenheit * windSpeedMph**0.1;
-
-if(farenheit <= 50 && windspeed > 3){
-    wind.textContent = `${windChill.toFixed(2)} F`;
-}
-else{
-    wind.textContent = "N/A";
-}
+// if(temperature <= 50 && windspeed > 3){
+//     const windChill = 35.74 + 0.6215 * temperature - 35.75 * windspeed**0.16 + 0.4275 * temperature * windspeed**0.1;
+//     document.querySelector('#windchill').textContent = windChill.toFixed(2);
+//     console.log(temperature);
+// }
+// else{
+//     document.querySelector('#windchill').textContent = 'N/A';
+// }
